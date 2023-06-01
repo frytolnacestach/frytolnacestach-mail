a863e7d2590e7fb42d6bd9ce0a150862ab04ad1c
frytolnacestach <frytolnacestach@gmail.com>
2023-06-01 09:52:52 +0200

    TEST - Email send 11

--- api/registration.js
+++ api/registration.js
@@ -6,36 +6,30 @@ const router = express.Router();
 router.get("/", async (req, res) => {
 
     const html = `
-        <h1> Hello mail - test 10</h1>
+        <h1> Hello mail - test 11</h1>
     `;
 
-    async function main() {
-        const transporter = nodeMailer.createTransport({
-            host: 'smtp.forpsi.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'registrace@frytolnacestach.cz',
-                pass: process.env.EMAIL_REG_PASS
-            }
-        });
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.forpsi.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'registrace@frytolnacestach.cz',
+            pass: process.env.EMAIL_REG_PASS
+        }
+    });
 
-        const info = await transporter.sendMail({
-            from: 'Registrace - Frytol na cestách <registrace@frytolnacestach.cz>',
-            to: 'frytolnacestach@gmail.com',
-            subject: 'Registrace - Frytol na cestách',
-            html: html,
-        })
+    const info = await transporter.sendMail({
+        from: 'Registrace - Frytol na cestách <registrace@frytolnacestach.cz>',
+        to: 'frytolnacestach@gmail.com',
+        subject: 'Registrace - Frytol na cestách',
+        html: html,
+    })
 
-        console.log("Message sent: " + info.messageId);
-        console.log(info.accepted);
-        console.log(info.rejected);
+    console.log("Message sent: " + info.messageId);
+    console.log(info.accepted);
+    console.log(info.rejected);
 
-        res.status(200).send("E-mail byl úspěšně odeslán.");
-    }
-
-    main()
-    .catch(e => console.log(e))
 
 });
 
