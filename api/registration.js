031dabfef1de07ca9dca8743a527790f6bc418e6
frytolnacestach <frytolnacestach@gmail.com>
2023-06-01 09:57:32 +0200

    TEST - Email send 12

--- api/registration.js
+++ api/registration.js
@@ -1,39 +1,56 @@
-const nodeMailer = require('nodemailer')
-
+const nodeMailer = require('nodemailer');
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
 
     const html = `
-        <h1> Hello mail - test 11</h1>
+        <h1> Hello mail - test 12</h1>
     `;
 
-    const transporter = nodeMailer.createTransport({
-        host: 'smtp.forpsi.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'registrace@frytolnacestach.cz',
-            pass: process.env.EMAIL_REG_PASS
+    function sendEmail(callback) {
+        const transporter = nodeMailer.createTransport({
+            host: 'smtp.forpsi.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'registrace@frytolnacestach.cz',
+                pass: process.env.EMAIL_REG_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: 'Registrace - Frytol na cestách <registrace@frytolnacestach.cz>',
+            to: 'frytolnacestach@gmail.com',
+            subject: 'Registrace - Frytol na cestách',
+            html: html,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("Error sending email:", error);
+                callback(error);
+            } else {
+                console.log("Message sent: " + info.messageId);
+                console.log(info.accepted);
+                console.log(info.rejected);
+                callback(null);
+            }
+        });
+    }
+
+    sendEmail((error) => {
+        if (error) {
+            res.status(500).send("Nepodařilo se odeslat e-mail.");
+        } else {
+            res.status(200).send("E-mail byl úspěšně odeslán.");
         }
     });
-
-    const info = await transporter.sendMail({
-        from: 'Registrace - Frytol na cestách <registrace@frytolnacestach.cz>',
-        to: 'frytolnacestach@gmail.com',
-        subject: 'Registrace - Frytol na cestách',
-        html: html,
-    })
-
-    console.log("Message sent: " + info.messageId);
-    console.log(info.accepted);
-    console.log(info.rejected);
-
 
 });
 
 module.exports = router;
+
 
 
 /*
