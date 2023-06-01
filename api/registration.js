5d95f2c88e3f28408c5a57bb61bcbfb82970988a
frytolnacestach <frytolnacestach@gmail.com>
2023-06-01 09:28:27 +0200

    TEST - Email send 9

--- api/registration.js
+++ api/registration.js
@@ -6,6 +6,49 @@ const router = express.Router();
 router.get("/", async (req, res) => {
 
     const html = `
+        <h1> Hello mail - test 9</h1>
+    `;
+
+    async function main() {
+        const transporter = nodeMailer.createTransport({
+            host: 'smtp.forpsi.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'registrace@frytolnacestach.cz',
+                pass: process.env.EMAIL_REG_PASS
+            }
+        });
+
+        const info = await transporter.sendMail({
+            from: 'Registrace - Frytol na cestách <registrace@frytolnacestach.cz>',
+            to: 'frytolnacestach@gmail.com',
+            subject: 'Registrace - Frytol na cestách',
+            html: html,
+        })
+
+        console.log("Message sent: " + info.messageId);
+        console.log(info.accepted);
+        console.log(info.rejected);
+    }
+
+    main()
+    .catch(e => console.log(e))
+
+});
+
+module.exports = router;
+
+
+/*
+const nodeMailer = require('nodemailer')
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+
+    const html = `
         <h1> Hello mail - test 8</h1>
     `;
 
@@ -35,4 +78,4 @@ router.get("/", async (req, res) => {
 
 });
 
-module.exports = router;
+module.exports = router;*/
